09a492dd804ef8f137624d14a10c36de6adb96c2
Brad Winter <bradleyswinter@hotmail.co.uk>
2020-11-25 08:42:14 +0000

    Added java executable in seperate thread

--- compiler.java
+++ compiler.java
@@ -1,15 +1,21 @@
 import javax.tools.*;
+import java.io.*;
 
 class compiler {
-  public static void main(String[] args) {
-    // TODO: Implement
+  public static void main(String[] args) throws IOException, InterruptedException {
     String file = "example.java";
+    String line;
     // compile program
     JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
     int result = compiler.run(null, null, null, file);
     // print result (successful output or error code)
     if (result == 0) {
       System.out.println("Compilation successful");
+      // If successful, run program
+      String[] command = {"java", "example"};
+      ProcessBuilder builder = new ProcessBuilder(command);
+      builder.inheritIO(); // redirects output to console
+      Process p =  builder.start();
     } else {
       System.out.println("Compilation failed");
     }
