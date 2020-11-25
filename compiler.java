49b2625d195937466ae50ab6c506afb6461de21c
Brad Winter <bradleyswinter@hotmail.co.uk>
2020-11-25 08:17:04 +0000

    Added compiler backend service

--- compiler.java
+++ compiler.java
@@ -1,8 +1,18 @@
+import javax.tools.*;
+
 class compiler {
   public static void main(String[] args) {
     // TODO: Implement
+    String file = "example.java";
     // compile program
+    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    int result = compiler.run(null, null, null, file);
     // print result (successful output or error code)
+    if (result == 0) {
+      System.out.println("Compilation successful");
+    } else {
+      System.out.println("Compilation failed");
+    }
   }
 }
 
